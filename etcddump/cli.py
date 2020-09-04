692014d8b04f3d8c5956d781d88ee681d3d5582f
Youhei Sakurai <sakurai.youhei@gmail.com>
2020-09-04 12:24:10 +0900

    sort out command line options

--- etcddump/cli.py
+++ etcddump/cli.py
@@ -1,26 +1,45 @@
-from . import operations
 import argparse
+from os.path import basename
+from sys import argv
+from sys import version_info
+from time import gmtime
+
+from etcddump import operations
+
+
+def version():
+    try:
+        from pkg_resources import get_distribution
+        pkgver = get_distribution("etcddump").version
+    except Exception:
+        pkgver = '.'.join(map(str, gmtime()[:3]))
+
+    return "{} v{} from {} (Python {}.{}.{})".format(
+        basename(argv[0]), pkgver, __file__, *version_info[:3])
 
 
 def main(**kw):
     parser = argparse.ArgumentParser(**kw)
-    parser.add_argument('--file',
+    parser.add_argument('-f', '--file',
                         default=None,
-                        help='File where the dump is located')
-    parser.add_argument('--preserve-indexes',
+                        help='File where the dump is located.')
+    parser.add_argument('-v', '--version',
+                        action='version',
+                        version=version(),
+                        help='Show version information.')
+    parser.add_argument('-p', '--preserve-indexes',
                         dest='preserve_indexes',
                         action='store_true',
                         help='Try to keep the same indexes as before.')
     parser.add_argument('action',
                         help='What to do: dump or restore')
-    parser.add_argument('host',
-                        default='http://localhost:4001',
-                        help='full url of the etcd host to act upon')
+    parser.add_argument('url',
+                        help='client url such as http://localhost:2379.')
     args = parser.parse_args()
 
     if args.action == 'dump':
-        cl = operations.Dumper(url=args.host)
+        cl = operations.Dumper(url=args.url)
         cl.dump(filename=args.file)
     elif args.action == 'restore':
-        cl = operations.Restorer(url=args.host)
+        cl = operations.Restorer(url=args.url)
         cl.restore(filename=args.file, preserve_indexes=args.preserve_indexes)
